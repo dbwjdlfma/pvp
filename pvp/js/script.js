ff8956fb366e92f405ea4ab6c7762e0de5970488
SMM2 <76415076+dbwjdlfma@users.noreply.github.com>
2025-07-05 19:04:03 +0900

    Update script.js

--- pvp/js/script.js
+++ pvp/js/script.js
@@ -3,7 +3,7 @@
 function displayVersion() {
   const version = document.getElementById("version");
   if (version) {
-    version.textContent = "V1.4.1";
+    version.textContent = "V1.4.2";
     Object.assign(version.style, {
       fontSize: "0.9em",
       position: "absolute",
